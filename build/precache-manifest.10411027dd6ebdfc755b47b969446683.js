c0755a279984a78ab352e9236ab00c56f6e09506
Antti Paavola <antti.paavis@gmail.com>
2020-02-05 18:58:22 +0200

    front-end build added

---
+++ build/precache-manifest.10411027dd6ebdfc755b47b969446683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cba16118d4708c1fa7c4172d91b7dcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "740d0020a79c52661cd2",
+    "url": "/static/css/main.67c31f5e.chunk.css"
+  },
+  {
+    "revision": "38a46c9f2ba18c850a01",
+    "url": "/static/js/2.3b795554.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.3b795554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740d0020a79c52661cd2",
+    "url": "/static/js/main.80a7cea9.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(express.static('build'))
 
 
 morgan.token('post', (req, res) => {
